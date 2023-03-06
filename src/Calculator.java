82d03da1e3c5f6bb7a866063d1db9d27431a2b36
Loveta <Loveta@inbox.lv>
2023-03-06 21:33:22 +0200

    Calculator input

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Calculator {
     // psvm shortcut for main function
     public static void main(String[] args) {
         System.out.println("Calculator...");
+        System.out.println("Calculating some of two numbers");
+        System.out.println("Provide first number: ");
+        int firstNumber;
+        int secondNumber;
+        //TODO: explain object creation
+        Scanner inputReader = new Scanner(System.in);
+        firstNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + firstNumber);
+        System.out.println("Provide second number: ");
+        secondNumber = inputReader.nextInt();
+        System.out.println("You provided number: " +secondNumber);
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum is: " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Difference is: " + difference);
+
+        int product = firstNumber * secondNumber;
+        System.out.println("Product is: " + product);
+
+        double quotient = firstNumber / secondNumber;
+        System.out.println("Quotient is: " + quotient);
     }
 }
