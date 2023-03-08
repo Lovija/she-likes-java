51a0da774e4e1935313cc23805e82ad6a096934b
Loveta <Loveta@inbox.lv>
2023-03-08 19:22:16 +0200

    packages introduced

--- src/com/sda/she_likes_java/homework/Calculator.java
+++ src/com/sda/she_likes_java/homework/Calculator.java
@@ -1,9 +1,11 @@
+package com.sda.she_likes_java.homework;
+
 import java.util.Scanner;
 
 public class Calculator {
     // psvm shortcut for main function
     public static void main(String[] args) {
-        System.out.println("Calculator...");
+        System.out.println("com.sda.she_likes_java.homework.Calculator...");
         System.out.println("Calculating some of two numbers");
         System.out.println("Provide first number: ");
         double firstNumber;
