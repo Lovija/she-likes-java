8ae957e96ec7b89794215f912bfd287d56f2b0bf
Loveta <Loveta@inbox.lv>
2023-03-08 20:37:31 +0200

    number ranges explained

---
+++ src/com/sda/she_likes_java/numbers/IntegerRanges.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.numbers;
+
+import java.sql.SQLOutput;
+
+public class IntegerRanges {
+    public static void main(String[] args) {
+        System.out.println("Let`s see how integers ranges looks like");
+        System.out.println("int ranges");
+        System.out.println("minimal int number is: " + Integer.MIN_VALUE);
+        System.out.println("maximum int number is: " + Integer.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("now let`s check long type");
+        System.out.println("minimal long number is: " + Long.MIN_VALUE);
+        System.out.println("maximal long number is: " + Long.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("now let`s check out short type");
+        System.out.println("minimal short number is: " + Short.MIN_VALUE);
+        System.out.println("maximum short number is: " + Short.MAX_VALUE);
+
+
+        System.out.println();
+
+        System.out.println("now let`s check byte type");
+        System.out.println("minimal byte number is: "+ Byte.MIN_VALUE);
+        System.out.println("maximum byte number is: "+Byte.MAX_VALUE);
+    }
+}
