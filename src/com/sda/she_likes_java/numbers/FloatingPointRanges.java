a1b578e1d80495281f086dfbf676e62085537442
Loveta <Loveta@inbox.lv>
2023-03-08 21:07:02 +0200

    float and double number ranges

---
+++ src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
@@ -0,0 +1,19 @@
+package com.sda.she_likes_java.numbers;
+//names of the class should start with capital letter always
+//names of variable should start with lower case letter
+public class FloatingPointRanges {
+    public static void main(String[] args) {
+        System.out.println("Floating numbers...");
+        System.out.println("float number");
+        System.out.println("minimal float number is: "+Float.MIN_VALUE);
+        //1.4E-45
+        //0.00000000000000000000000001 E stands for how much 0
+        System.out.println("maximal float number is: "+Float.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("double number");
+        System.out.println("minimal double number is: "+Double.MIN_VALUE);
+        System.out.println("maximum double number is: "+Double.MAX_VALUE);
+    }
+}
