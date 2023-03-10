dc0e46e9e74283d6174af491faa8881aa5db4155
Loveta <Loveta@inbox.lv>
2023-03-10 21:20:35 +0200

    Literals

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is direct value
+        // String literal is  for example "name"
+        String name = "Loveta";
+
+        //integer literal
+        int age = 40; // 40 is of type int
+
+        //long literar
+        long anotherAge= 30L;
+
+        //double literar
+        double doubleNumber = 3.1;
+
+        //float literar
+        float floatNumber = 3.1F;
+    }
+}
