eda60652b31b5fbf6f53e2c5b352495fba52448f
Loveta <Loveta@inbox.lv>
2023-03-10 15:31:03 +0200

    homework BouncerInDisco

---
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class BouncerInDisco {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("How old are you: ");
+        int personAge = inputReader.nextInt();
+        System.out.println();
+        System.out.println("How m tall are you: ");
+        double personHeight = inputReader.nextDouble();
+        System.out.println();
+        if (personAge>=16&personHeight>=1.55){
+            System.out.println("Hello disco dancer!");
+        }
+        else {
+            System.out.println("Go party at your house!");
+        }
+
+
+    }
+}
