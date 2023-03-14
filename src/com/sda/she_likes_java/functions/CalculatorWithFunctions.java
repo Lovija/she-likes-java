5f67302c8b0286a6c7f1b715911a4772fc07bc50
Loveta <Loveta@inbox.lv>
2023-03-14 19:53:46 +0200

    functions calculation

---
+++ src/com/sda/she_likes_java/functions/CalculatorWithFunctions.java
@@ -0,0 +1,16 @@
+package com.sda.she_likes_java.functions;
+
+public class CalculatorWithFunctions {
+    public static void main(String[] args) {
+        System.out.println("Let`s start calculations");
+        sum(15, 28);
+       sum(1,2);
+    }
+
+    public static int sum(int first, int second) {
+        System.out.println("I`m smart or not so smart calculator");
+        int result= first +second;
+        System.out.println("Result of calculation is: "+result);
+        return result;
+    }
+}
