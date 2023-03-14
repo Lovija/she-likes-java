6d217ee20be105743383306814b4e620f5ade925
Loveta <Loveta@inbox.lv>
2023-03-14 19:01:20 +0200

    do while loop

---
+++ src/com/sda/she_likes_java/loops/do_while/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.loops.do_while;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+       //boolean isAdult = false;
+       // do {
+        //    System.out.println("Can you see this line?");
+       // } while (isAdult);
+        boolean isAdult = true;
+        do {
+            System.out.println("Can you read this line?");
+        }
+        while (isAdult);
+    }
+}
