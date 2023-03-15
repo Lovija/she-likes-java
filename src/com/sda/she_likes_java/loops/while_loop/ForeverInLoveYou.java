fd264dee99cb9a51612e10009976cab2343557f9
Loveta <Loveta@inbox.lv>
2023-03-15 21:22:15 +0200

    Break the loop

---
+++ src/com/sda/she_likes_java/loops/while_loop/ForeverInLoveYou.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class ForeverInLoveYou {
+    public static void main(String[] args) {
+        int numberOfTrials = 0;
+        while (true){
+            System.out.println("Forever in love :)");
+            numberOfTrials++;
+            if (numberOfTrials >=10) {
+                break;
+            }
+        }
+    }
+}
