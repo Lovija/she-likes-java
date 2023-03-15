3ce6578d8024b474235c1d6b9748a05922957dd7
Loveta <Loveta@inbox.lv>
2023-03-15 21:33:01 +0200

    Game with brake

---
+++ src/com/sda/she_likes_java/loops/do_while/GuessTheNumberGameWithBrake.java
@@ -0,0 +1,32 @@
+package com.sda.she_likes_java.loops.do_while;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumberGameWithBrake {
+    public static void main(String[] args) {
+        //random makes random number in console
+        Random numberGenerator = new Random();
+        //when you write number in brackets nextInt, then shows name bound
+        int someRandomNumber = numberGenerator.nextInt(20);
+        int numberOfTrials = 4;
+        int userAnswer;
+       //System.out.println("Random number provided by java is: "+someRandomNumber);
+        //generate one more number and user needs to
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Play game with me");
+
+        do  {
+            if (numberOfTrials <=0 ){
+                break;
+            }
+            System.out.println("Guess a number between 1 and 20");
+            userAnswer =inputReader.nextInt();
+            numberOfTrials--;
+        }
+        while (userAnswer!=someRandomNumber);
+        //!= means different
+
+        System.out.println("Great, You guessed it! :)");
+    }
+}
