876545066a4a76b1f11f6287042d82dfbcd6edc6
Loveta <Loveta@inbox.lv>
2023-03-27 22:43:37 +0300

    For and while loop

---
+++ src/com/sda/she_likes_java/loops/for_loop/ForLoopExample.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.loops.for_loop;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // we need to print 100 numbers
+        for (
+                int number = 0; //initialization part and executed only once
+                number < 100; // condition of loop, will be executed while number is smaller than 100
+                number ++ // executed before condition
+        ){
+            System.out.println("Number value is: " + number);
+        }
+
+
+        int number  = 0;
+        while (number<100) {
+            System.out.println("Number value is: "+number);
+            number++;
+        }
+    }
+}
