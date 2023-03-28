58eb55d32c286a1291a55f30595054ec4a38a987
Loveta <Loveta@inbox.lv>
2023-03-28 18:47:05 +0300

    Homework exercise 15

---
+++ src/com/sda/she_likes_java/homework/exercise15/Parent.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.homework.exercise15;
+
+import com.sda.she_likes_java.classes.Address;
+import com.sda.she_likes_java.classes.Person;
+
+public class Parent {
+    String firstName;
+    String surname;
+    Person firstChild;
+    Person secondChild;
+    Address address;
+}
---
+++ src/com/sda/she_likes_java/homework/exercise15/Person.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.homework.exercise15;
+
+public class Person {
+    String name;
+    String surname;
+    int age;
+   Person firstChild;
+   Person secondChild;
+   Address address;
+}
+
