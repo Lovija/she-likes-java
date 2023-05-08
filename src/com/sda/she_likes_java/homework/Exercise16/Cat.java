f87882b2f350adb4a152ab5ca9e4fe0a0dce552e
Loveta <Loveta@inbox.lv>
2023-05-08 19:31:39 +0300

    Exercise16 homework

---
+++ src/com/sda/she_likes_java/homework/Exercise16/Cat.java
@@ -0,0 +1,17 @@
+package com.sda.she_likes_java.homework.Exercise16;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
