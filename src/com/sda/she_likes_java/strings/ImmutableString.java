f5454c1b64d3b409368602c88545c16d827f133a
Loveta <Loveta@inbox.lv>
2023-05-09 22:08:00 +0300

    ImmutableString example

---
+++ src/com/sda/she_likes_java/strings/ImmutableString.java
@@ -0,0 +1,9 @@
+package com.sda.she_likes_java.strings;
+
+public class ImmutableString {
+    public static void main(String[] args) {
+        String name = "Loveta";
+        name.concat("p"); //  concat means the same as this name + "p"
+        System.out.println("My name now: "+name);
+    }
+}
