a4cb43ddacefe947fd6ca5fe68421f366942d303
Loveta <Loveta@inbox.lv>
2023-05-09 23:10:33 +0300

    Polymorphism string example

---
+++ src/com/sda/she_likes_java/polymorphism/Cat.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class Cat extends Animal {
+    public void makeSound(){
+        System.out.println("Mjau");
+    }
+public void sleep(){
+    System.out.println("Cat is sleeping");
+}
+}
