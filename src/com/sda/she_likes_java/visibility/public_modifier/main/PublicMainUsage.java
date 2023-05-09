e69ee000b999a0cf6d362b8e85497017edbc3fe4
Loveta <Loveta@inbox.lv>
2023-05-09 18:37:06 +0300

    public modifier - introduction

---
+++ src/com/sda/she_likes_java/visibility/public_modifier/main/PublicMainUsage.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.visibility.public_modifier.main;
+
+import com.sda.she_likes_java.visibility.public_modifier.drivers.ProfessionalDriver;
+
+public class PublicMainUsage {
+    public static void main(String[] args) {
+        ProfessionalDriver me = new ProfessionalDriver();
+        //if the class is public, you can see this class outside it`s package
+
+    }
+}
