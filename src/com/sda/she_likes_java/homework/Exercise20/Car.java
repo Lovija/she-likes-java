10582080030e5ad990e528e03bf1c122905e2687
Loveta <Loveta@inbox.lv>
2023-05-10 18:44:19 +0300

    Exercise 20

---
+++ src/com/sda/she_likes_java/homework/Exercise20/Car.java
@@ -0,0 +1,7 @@
+package com.sda.she_likes_java.homework.Exercise20;
+
+public class Car {
+    public void drive(){
+        System.out.println("Car is driving");
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/Exercise20/Main.java
@@ -0,0 +1,9 @@
+package com.sda.she_likes_java.homework.Exercise20;
+
+public class Main {
+    public static void main(String[] args) {
+        CarUtils.safeCast(new Mazda());
+        CarUtils.safeCast(new Ferrari());
+        CarUtils.safeCast(new Toyota());
+    }
+}
