b7c9591c1f01181d2a07ea9ec22429a63a3b28ed
Loveta <Loveta@inbox.lv>
2023-05-10 20:41:28 +0300

    deleting package

--- src/com/sda/she_likes_java/common_behaviour/Swimmable.java
+++ src/com/sda/she_likes_java/common_behaviour/Swimmable.java
@@ -1,4 +1,4 @@
-package com.sda.she_likes_java.common_behaviour.wrong_approach;
+package com.sda.she_likes_java.common_behaviour;
 
 public interface Swimmable {
  void swim();// we don`t need to write word public because it is here by default
