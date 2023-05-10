c791082c1af4b5dbe094ddb0e36489f0d500af82
Loveta <Loveta@inbox.lv>
2023-05-10 12:29:55 +0300

    Instanceof introduction

---
+++ src/com/sda/she_likes_java/virtual_methods/HumanLife.java
@@ -0,0 +1,25 @@
+package com.sda.she_likes_java.virtual_methods;
+
+public class HumanLife {
+    public void tellMeWhoYouAre(Human human){
+        if (human !=null) {
+            human.whoYouAre();
+        }
+    }
+    public void tryToCastHumanToMan(Human maybeWomanOrMan){
+        if (maybeWomanOrMan instanceof Man){
+            //instanceof will check if the object is man or woman true/false
+            // if it is man, then casting is happening
+       //casting or down-casting from generic to specific
+        // casting works for references
+
+            ((Man) maybeWomanOrMan).learnToCleanHouse();
+        }
+       // ((Woman)maybeWomanOrMan).learnJava(); //if we want to get to learn java from woman class and it doesn`t work, because man and woman are different classes
+        // every woman is a human, but not  every human is woman
+        if (maybeWomanOrMan instanceof Woman) {
+            ((Woman) maybeWomanOrMan).learnJava();
+        }
+        }
+
+}
--- src/com/sda/she_likes_java/virtual_methods/Main.java
+++ src/com/sda/she_likes_java/virtual_methods/Main.java
@@ -4,5 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Mammal human = new Human();
         human.whoYouAre();
+        Human noHuman = null;
+        HumanLife humanLife = new HumanLife();
+        humanLife.tellMeWhoYouAre(null);
+
+        Woman woman = new Woman();
+        humanLife.tellMeWhoYouAre(woman);
+
+        Man man = new Man();
+        humanLife.tellMeWhoYouAre(man);
+
+        System.out.println("Casting objects");
+        Human anotherWoman = new Woman();
+        Woman castedFromHuman = (Woman) anotherWoman;
+        castedFromHuman.learnJava();
+        Man manForSure = new Man();
+        Human anotherMan = manForSure;
+        //humanLife.tryToCastHumanToMan(anotherMan);
+        humanLife.tryToCastHumanToMan(new Man());
+        //humanLife.tryToCastHumanToMan(new Woman());
     }
 }
